3c770a840325e1198a6df5240644a5cc5b430f44
byQbit <byqubit@gmail.com>
2024-04-18 17:33:06 -0400

    Updated stock_lists

--- stocks/stock_lists.py
+++ stocks/stock_lists.py
@@ -2,20 +2,114 @@
 # stock_lists.py
 
 # List of individual stocks
+
 all_stocks = [
-    "NVDA",
-    "AAPL",
-    "GOOGL",
-    "TSLA",
-    "MSFT",
-    "AMZN",
-    "NFLX",
-    "META",
-    "TWTR",
-    ]
+
+    "AAPL",   # Apple Inc.
+    "MSFT",   # Microsoft Corporation
+    "AMZN",   # Amazon.com Inc.
+    "GOOGL",  # Alphabet Inc. (Google)
+    "FB",     # Meta Platforms Inc. (Facebook)
+    "TSLA",   # Tesla, Inc.
+    "BRK.A",  # Berkshire Hathaway Inc. Class A
+    "JPM",    # JPMorgan Chase & Co.
+    "JNJ",    # Johnson & Johnson
+    "V",      # Visa Inc.
+    "PG",     # Procter & Gamble Company
+    "NVDA",   # NVIDIA Corporation
+    "HD",     # The Home Depot, Inc.
+    "MA",     # Mastercard Incorporated
+    "UNH",    # UnitedHealth Group Incorporated
+    "PYPL",   # PayPal Holdings, Inc.
+    "BAC",    # Bank of America Corporation
+    "DIS",    # The Walt Disney Company
+    "ADBE",   # Adobe Inc.
+    "CMCSA",  # Comcast Corporation
+    "INTC",   # Intel Corporation
+    "CRM",    # Salesforce.com Inc.
+    "KO",     # The Coca-Cola Company
+    "NFLX",   # Netflix, Inc.
+    "PFE",    # Pfizer Inc.
+    "ABBV",   # AbbVie Inc.
+    "VZ",     # Verizon Communications Inc.
+    "T",      # AT&T Inc.
+    "MRK",    # Merck & Co., Inc.
+    "CSCO",   # Cisco Systems, Inc.
+    "XOM",    # Exxon Mobil Corporation
+    "PEP",    # PepsiCo, Inc.
+    "WMT",    # Walmart Inc.
+    "WFC",    # Wells Fargo & Company
+    "CMG",    # Chipotle Mexican Grill, Inc.
+    "AMD",    # Advanced Micro Devices, Inc.
+    "NKE",    # NIKE, Inc.
+    "GE",     # General Electric Company
+    "BABA",   # Alibaba Group Holding Limited
+    "IBM",    # International Business Machines Corporation
+    "ROKU",   # Roku, Inc.
+    "SNAP",   # Snap Inc.
+    "SQ",     # Square, Inc.
+    "UBER",   # Uber Technologies, Inc.
+    "LYFT",   # Lyft, Inc.
+    "ZM",     # Zoom Video Communications, Inc.
+    "BYND",   # Beyond Meat, Inc.
+    "SPOT",   # Spotify Technology S.A.
+    "GOEV",   # Canoo Inc.
+
+]
+
 
 # List of stock indices
+
 all_indices = [
-    "^DJI",
-    "^GSPC",
-    "^IXIC"]  # Dow Jones Industrial Average, S&P 500, Nasdaq
+    
+    "^DJI",   # Dow Jones Industrial Average
+    "^GSPC",  # S&P 500
+    "^IXIC",  # Nasdaq
+    "^FTSE",  # FTSE 100
+    "^N225",  # Nikkei 225
+    "^HSI",   # Hang Seng Index
+    "^GDAXI", # DAX Performance Index
+    "^FCHI",  # CAC 40
+    "^IBEX",  # IBEX 35
+    "^BVSP",  # Bovespa Index
+    "^AORD",  # S&P/ASX 200
+    "^BSESN", # BSE SENSEX
+    "^TWII",  # TSEC weighted index
+    "^KS11",  # KOSPI Index
+    "^SSMI",  # Swiss Market Index
+    "^MXX",   # IPC Mexico
+    "^OSEAX", # Oslo Exchange All Share Index
+    "^STI",   # Straits Times Index
+    "^TA125.TA",  # TA-125 Index
+    "^OMX",   # OMX Stockholm 30
+    "^OSEAX", # Oslo Exchange All Share Index
+    "^JKSE",  # Jakarta Composite Index
+    "^MERV",  # MERVAL
+    "^NZ50",  # NZX 50
+    "^J203.L",# JSE Top 40
+    "^N100",  # EURONEXT 100
+    "^SENSEX",# S&P BSE SENSEX
+    "^PSI20", # PSI 20
+    "^STOXX50E", # EURO STOXX 50
+    "^ATX",   # ATX
+    "^XU100", # BIST 100
+    "^FTSEMIB.MI", # FTSE MIB
+    "^N225",  # Nikkei 225
+    "^GSPTSE",# S&P/TSX Composite Index
+    "^BFX",   # BEL 20
+    "^OSEAX", # Oslo Exchange All Share Index
+    "^OMXHPI",# OMX Helsinki PI
+    "^ISEQ",  # ISEQ Overall Index
+    "^XU100", # BIST 100
+    "^OSEAX", # Oslo Exchange All Share Index
+    "^MERV",  # MERVAL
+    "^BUX",   # BUX Budapest
+    "^FTSEMIB.MI", # FTSE MIB
+    "^GDAXI", # DAX Performance Index
+    "^OSEAX", # Oslo Exchange All Share Index
+    "^J203.L",# JSE Top 40
+    "^NZ50",  # NZX 50
+    "^OMX",   # OMX Stockholm 30
+    "^N225"   # Nikkei 225
+]
+
