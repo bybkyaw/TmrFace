533ca8695b2bffea4c7c8bc25d14bf289c7cc942
byQbit <byqubit@gmail.com>
2024-04-25 17:27:04 -0400

    Updated Sentiment Analysis & Ranking

--- news/col_2_news.py
+++ news/col_2_news.py
@@ -13,7 +13,7 @@ from news.news_scraper import scrape_general_news
 from stocks.stock_lists import all_stocks
 from news.sentiment_plotting import plot_sentiment_scores
 from news.news_custom_scraper import find_sentences_with_keywords, scrape_custom_news_with_keywords
-from news.sentiment_display import display_sentiment_scores_dropdown
+from news.sentiment_display import display_sentiment_scores_dropdown, display_text_count_dropdown
 
 # Your Finnhub API key
 API_KEY = 'cogmlnpr01qj0pq2pnk0cogmlnpr01qj0pq2pnkg'
@@ -56,23 +56,6 @@ def scrape_and_process_news():
     return news_data
 
 
-
-# def scrape_and_process_news():
-#     # Call the scraping function to get raw news data
-#     scraped_articles = scrape_general_news()
-
-#     # Process each article to analyze sentiment and score
-#     news_data = []
-#     for article in scraped_articles:
-#         sentiment, score = analyze_sentiment(article['text'])
-#         news_data.append({
-#             "text": article['text'],
-#             "sentiment": sentiment,
-#             "score": score
-#         })
-
-#     return news_data
-
 def display_news_headlines(news_data):
     
     news_headlines = ""
@@ -102,9 +85,29 @@ def display_col2(selected_stock):
             #display_sentiment_analysis(general_news)
             display_sentiment_scores_dropdown(general_news)
 
+        st.markdown("<hr>", unsafe_allow_html=True)
+
+         # Expander for Article Ranking
+        with st.expander("Article Ranking", expanded=False):
+            sentiment_data = [{"Article Number": i + 1, "Sentiment": article['sentiment']['label'], 
+                               "Score": article['sentiment']['score']} for i, article in enumerate(general_news)]
+            sentiment_df = pd.DataFrame(sentiment_data)
+            
+            # Sort sentiment_df by score in descending order
+            sentiment_df = sentiment_df.sort_values(by='Score', ascending=False)
+            st.table(sentiment_df)
+        
+        st.markdown("<hr>", unsafe_allow_html=True)
+    
+        with st.expander("Text Count"):
+            display_text_count_dropdown(general_news)
+    else:
+        st.error("Failed to fetch general news.")
+
+
 
     st.markdown("<hr>", unsafe_allow_html=True)
-
+    
     if selected_stock:
         st.header(f"News for {selected_stock}")
         stock_news = fetch_stock_news(selected_stock)
@@ -140,108 +143,7 @@ def display_col2(selected_stock):
     st.markdown("<hr>", unsafe_allow_html=True)
 
 
-# import requests
-# import pandas as pd
-# import streamlit as st
-# from news.sentiment_analysis import analyze_sentiment
 
-# from news.news_scraper import scrape_general_news, scrape_stock_news
-# from stocks.stock_lists import all_stocks, all_indices
-# from news.news_custom_scraper import scrape_custom_news, scrape_custom_news_with_keywords, find_sentences_with_keywords
-
-
-# # Your Finnhub API key
-# API_KEY = 'cogmlnpr01qj0pq2pnk0cogmlnpr01qj0pq2pnkg'
-
-# def fetch_stock_news(symbol):
-#     """ Fetches the latest stock news for a given symbol from Finnhub API. """
-#     url = f'https://finnhub.io/api/v1/company-news?symbol={symbol}&from={pd.Timestamp.now().date()}&to={pd.Timestamp.now().date()}&token={API_KEY}'
-#     response = requests.get(url)
-#     return response.json() if response.status_code == 200 else []
-
-# def limit_sentences(text, max_sentences=3):
-#     sentences = text.split(".")
-#     return ". ".join(sentences[:max_sentences]) + ("." if len(sentences) > 2 else "")
-
-# def display_news_bullet_points(news_list):
-#     for news in news_list[:6]:  # Displaying only the first 6 news items
-#         if 'summary' in news and news['summary']:  # Check if summary exists and is not empty
-#             limited_text = limit_sentences(news['summary'])
-#             st.markdown(f"- **[{news['headline']}]({news['url']})**: {limited_text}")
-
-# def display_col2(selected_stock):
-#     st.header("Today's Global News")
-
-#     # Function to limit the text to the first 3 sentences
-#     def limit_sentences1(text, max_sentences=3):
-#         sentences = text.split(".")
-#         return ". ".join(sentences[:max_sentences]) + ("." if len(sentences) > 2 else "")
-
-#     # Function to display news articles in bullet points, each limited to 3 sentences
-
-#     def display_news_bullet_points1(news_list):
-
-#         for i, news in enumerate(news_list[:6]):  # Limit to maximum 6 bullet points
-#             sentiment = analyze_sentiment(news['text'])
-#             color = "#ADD8E6" if sentiment == "positive" else "#F08080" if sentiment == "negative" else "#D3D3D3"
-#             limited_text = limit_sentences1(news['text'])
-#             st.markdown(f"- <p style='color: {color};'> {limited_text}</p>", unsafe_allow_html=True)
-
-    
-#     # Scrape and display general news for all stocks in bullet points
-#     general_news = scrape_general_news()
-
-#     if general_news:
-#         display_news_bullet_points1(general_news)
-#     else:
-#         st.warning("Failed to fetch general news.")
-
-#     # Add a horizontal divider
-#     st.markdown("<hr>", unsafe_allow_html=True)
-
-
-    
-
-
-#     # Add a horizontal divider
-#     st.markdown("<hr>", unsafe_allow_html=True)
-
-
-#     # Display news specific to the selected stock in bullet points, limited to 3 sentences each
-#     if selected_stock:
-#         st.header(f"News for {selected_stock}")
-#         stock_news = fetch_stock_news(selected_stock)
-
-#         if stock_news:
-#             display_news_bullet_points(stock_news)
-
-#         else:
-#             st.warning(f"No news found for {selected_stock}.")
-#     else:
-#         st.warning("No stock selected.")
-
-#      # Add a horizontal divider
-#     st.markdown("<hr>", unsafe_allow_html=True)
-
-#     st.subheader("Scrape Custom URL with Keywords")
-#     custom_url = st.text_input("Enter URL to scrape:")
-#     keywords = st.text_input("Enter keywords (comma-separated):")
-
-#     if st.button("Scrape"):
-#         if custom_url:
-#             custom_news = scrape_custom_news_with_keywords(custom_url, keywords)
-#             if custom_news:
-#                 st.write("Scraped Text:")
-#                 for news in custom_news[:3]:  # Limit to maximum 3 articles
-#                     sentences = find_sentences_with_keywords(news['text'], keywords)
-#                     for sentence in sentences:
-#                         st.markdown(f"- {sentence}")  # Display each sentence as a bullet point
-#             else:
-#                 st.warning("Failed to scrape custom news.")
-#         else:
-#             st.warning("Please enter a URL to scrape.")
-
-   
 
 
 
--- news/sentiment_display.py
+++ news/sentiment_display.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 def display_sentiment_scores_dropdown(displayed_news):
-
     if displayed_news:
-        st.header("Sentiment Analysis Scores")
+        st.header("Sentiment Analysis")
         selected_article = st.selectbox("Select Article", [f"Article {i + 1}" for i in range(len(displayed_news))])
 
         if selected_article:
@@ -12,15 +13,192 @@ def display_sentiment_scores_dropdown(displayed_news):
             selected_news = displayed_news[index]
             sentiment = selected_news['sentiment']
 
-            # Determine font color based on sentiment score
-            font_color = '#AFE1AF' if sentiment['score'] > 0 else '#ff9999' if sentiment['score'] < 0 else '#FCFBF4'
+            # Determine dot color based on sentiment score
+            dot_color = '#AFE1AF' if sentiment['score'] > 0 else '#ff9999' if sentiment['score'] < 0 else '#FCFBF4'
 
-            # Display sentiment analysis with font color using HTML
-            st.write(f"Sentiment Analysis for {selected_article}:")
+            # Display sentiment analysis with dot color using HTML
+            st.write(f"Sentiment Analysis Score for {selected_article}:")
             st.markdown(
                 f"<table><tr><th style='color: #FCFBF4;'>Sentiment</th><th style='color: #FCFBF4;'>Score</th></tr>"
-                f"<tr><td style='color: {font_color};'>{sentiment['label'].capitalize()}</td>"
-                f"<td style='color: {font_color};'>{sentiment['score']}</td></tr></table>",
+                f"<tr><td style='color: {dot_color};'>{sentiment['label'].capitalize()}</td>"
+                f"<td style='color: {dot_color};'>{sentiment['score']}</td></tr></table>",
                 unsafe_allow_html=True
             )
+        st.write("")
+        
+        # Checkbox to show/hide sentiment scores for all articles
+        show_scores = st.checkbox("Show sentiment scores for all articles")
+
+        if show_scores:
+            # Display table of sentiment scores for all articles
+            st.header("Sentiment Scores for All Articles")
+            sentiment_data = [{"Article Number": i + 1, "Sentiment": article['sentiment']['label'], 
+                               "Score": article['sentiment']['score']} for i, article in enumerate(displayed_news)]
+            sentiment_df = pd.DataFrame(sentiment_data)
+            st.table(sentiment_df)
+
+        st.markdown("<hr>", unsafe_allow_html=True)
+
+        # Display scatter plot for sentiment scores of all articles
+        st.header("Scatter Plot for All Articles")
+        all_sentiments = []
+        for i, article in enumerate(displayed_news):
+            all_sentiments.append({'Article Number': i + 1, 'Score': article['sentiment']['score'],
+                                   'Color': 'Positive' if article['sentiment']['score'] > 0
+                                            else 'Negative' if article['sentiment']['score'] < 0
+                                            else 'Neutral'})
+            
+        all_sentiments_df = pd.DataFrame(all_sentiments)
+
+        fig_scatter = px.scatter(all_sentiments_df, x='Article Number', y='Score', color='Color',
+                                 title='Sentiment Analysis Scatter Plot',
+                                 labels={'Article Number': 'Article Number', 'Score': 'Sentiment Score'},
+                                 hover_name='Article Number', hover_data={'Score': True},
+                                 render_mode='webgl')  
+        
+        fig_scatter.update_traces(mode='lines+markers', line=dict(color='#C0C0C0', width=1))  # Connect dots with lines
+        fig_scatter.update_layout(hovermode='closest')  # Show closest data on hover
+        st.plotly_chart(fig_scatter)
+
+        st.write("")
+        
+        st.markdown("<hr>", unsafe_allow_html=True)
+        
+        # # Expander to show/hide Article Ranking
+        # with st.expander("Article Ranking", expanded=False):
+        #     # Display table of sentiment scores for all articles
+        #     sentiment_data = [{"Article Number": i + 1, "Sentiment": article['sentiment']['label'], 
+        #                        "Score": article['sentiment']['score']} for i, article in enumerate(displayed_news)]
+        #     sentiment_df = pd.DataFrame(sentiment_data)
+            
+        #     # Sort sentiment_df by score in descending order
+        #     sentiment_df = sentiment_df.sort_values(by='Score', ascending=False)
+        #     st.table(sentiment_df)
+
+    else:
+        st.warning("No sentiment scores found in the news data.")
+
+
+# import streamlit as st
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import plotly.express as px
+
+
+# def display_sentiment_scores_dropdown(displayed_news):
+#     if displayed_news:
+#         st.header("Sentiment Analysis")
+#         selected_article = st.selectbox("Select Article", [f"Article {i + 1}" for i in range(len(displayed_news))])
+
+#         if selected_article:
+#             index = int(selected_article.split()[-1]) - 1
+#             selected_news = displayed_news[index]
+#             sentiment = selected_news['sentiment']
+
+#             # Determine dot color based on sentiment score
+#             dot_color = '#AFE1AF' if sentiment['score'] > 0 else '#ff9999' if sentiment['score'] < 0 else '#FCFBF4'
+
+#             # Display sentiment analysis with dot color using HTML
+#             st.write(f"Sentiment Analysis Score for {selected_article}:")
+#             st.markdown(
+#                 f"<table><tr><th style='color: #FCFBF4;'>Sentiment</th><th style='color: #FCFBF4;'>Score</th></tr>"
+#                 f"<tr><td style='color: {dot_color};'>{sentiment['label'].capitalize()}</td>"
+#                 f"<td style='color: {dot_color};'>{sentiment['score']}</td></tr></table>",
+#                 unsafe_allow_html=True
+#             )
+#         st.write("")
+#         # Checkbox to show/hide sentiment scores for all articles
+#         show_scores = st.checkbox("Show sentiment scores for sll srticles")
+
+#         if show_scores:
+#             # Display table of sentiment scores for all articles
+#             st.header("Sentiment Scores for All Articles")
+#             sentiment_data = [{"Article Number": i + 1, "Sentiment": article['sentiment']['label'], 
+#                                "Score": article['sentiment']['score']} for i, article in enumerate(displayed_news)]
+#             sentiment_df = pd.DataFrame(sentiment_data)
+#             st.table(sentiment_df)
+
+#         st.markdown("<hr>", unsafe_allow_html=True)
+
+#         # Display scatter plot for sentiment scores of all articles
+#         st.header("Scatter Plot for All Articles")
+#         all_sentiments = []
+#         for i, article in enumerate(displayed_news):
+
+#             all_sentiments.append({'Article Number': i + 1, 'Score': article['sentiment']['score'],
+#                                    'Color': 'Positive' if article['sentiment']['score'] > 0
+#                                             else 'Negative' if article['sentiment']['score'] < 0
+#                                             else 'Neutral'})
+            
+#         all_sentiments_df = pd.DataFrame(all_sentiments)
+
+#         fig_scatter = px.scatter(all_sentiments_df, x='Article Number', y='Score', color='Color',
+#                                  title='Sentiment Analysis Scatter Plot',
+#                                  labels={'Article Number': 'Article Number', 'Score': 'Sentiment Score'},
+#                                  hover_name='Article Number', hover_data={'Score': True},
+#                                  render_mode='webgl')  
+        
+#         fig_scatter.update_traces(mode='lines+markers', line=dict(color='#C0C0C0', width=1))  # Connect dots with lines
+#         fig_scatter.update_layout(hovermode='closest')  # Show closest data on hover
+#         st.plotly_chart(fig_scatter)
+
+#         st.write("")
+        
+#         st.markdown("<hr>", unsafe_allow_html=True)
+        
+#         # Checkbox to show/hide Article Ranking
+#         show_ranking = st.checkbox("Article Ranking")
+
+#         if show_ranking:
+#             # Display table of sentiment scores for all articles
+#             st.header("Article Ranking")
+#             sentiment_data = [{"Article Number": i + 1, "Sentiment": article['sentiment']['label'], 
+#                                "Score": article['sentiment']['score']} for i, article in enumerate(displayed_news)]
+#             sentiment_df = pd.DataFrame(sentiment_data)
+            
+#             # Sort sentiment_df by score in descending order
+#             sentiment_df = sentiment_df.sort_values(by='Score', ascending=False)
+#             st.table(sentiment_df)
+
+
+#     else:
+#         st.warning("No sentiment scores found in the news data.")
+
+
+
+
+
+def display_text_count_dropdown(general_news):
+    if general_news:
+        st.header("Scrapped Articles Text Count")
+        
+        # Create a list to hold text count data for each article
+        text_count_data = []
+
+        # Iterate over each article in general_news
+        for index, article in enumerate(general_news):
+
+            # Count the number of words in the article's text
+            text_count = len(article['text'].split())
+            # Append article number and text count to the list
+
+            text_count_data.append({"Article Number": index + 1, "Text Count": text_count})
+
+        # Create a DataFrame from the text count data
+        text_count_df = pd.DataFrame(text_count_data)
+
+        # Display the text count data in a table format
+        st.table(text_count_df)
+
+        # Plot the text count data as a hoverable bar chart using Plotly Express
+        fig = px.bar(text_count_df, x='Article Number', y='Text Count', 
+                     title='Text Count for Each Article',
+                     labels={'Article Number': 'Article Number', 'Text Count': 'Text Count'})
+        fig.update_layout(xaxis=dict(type='category'))  # Ensure article number is treated as category
+        st.plotly_chart(fig)
+
+    else:
+        st.warning("No articles available.")
+
+
 
